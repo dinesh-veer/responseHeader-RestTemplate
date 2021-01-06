adb6bcf766690130738fff90431707229ee4fca4
dineshveer <dveer123@hotmail.com>
2021-01-06 21:37:50 +0530

    Added code to get response and headers

--- src/main/java/com/springBoot/demo/ResponseHeaderRestTemplateApplication.java
+++ src/main/java/com/springBoot/demo/ResponseHeaderRestTemplateApplication.java
@@ -1,7 +1,16 @@
 package com.springBoot.demo;
 
+
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ResponseHeaderRestTemplateApplication {
@@ -10,4 +19,28 @@ public class ResponseHeaderRestTemplateApplication {
 		SpringApplication.run(ResponseHeaderRestTemplateApplication.class, args);
 	}
 
+	private static final org.slf4j.Logger log = LoggerFactory.getLogger(ResponseHeaderRestTemplateApplication.class);
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+		//	HttpEntity<String> response = restTemplate.exchange(
+		//			"https://gturnquist-quoters.cfapps.io/api/random", HttpMethod.GET,null ,String.class);
+		//	log.info(response.)
+				
+				HttpEntity<String> response = restTemplate.exchange("https://gturnquist-quoters.cfapps.io/api/random", HttpMethod.GET, null, String.class);
+
+				String resultString = response.getBody();
+				HttpHeaders headers = response.getHeaders();
+				log.info("Header", headers.toString());
+				log.info("body", resultString.toString());
+		};
+	}
+	
+	
 }
